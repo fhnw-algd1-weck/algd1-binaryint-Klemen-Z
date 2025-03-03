09671854b95c7765609008718ce86573ccb62d00
Klemen-Z <klemen.zmahar@students.fhnw.ch>
2025-03-03 08:54:20 +0100

    to string fixed

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -2,34 +2,12 @@ package ch.fhnw.algd1.converters.binary;
 
 public class BinConverter {
 	public static String toString(int x) {
-		StringBuilder output = new StringBuilder();
-		byte power = 0;
-		int counter = 0;
+		StringBuilder output = new StringBuilder(8);
 
-		byte temp = (byte) x;
-
-		if (x < 0){
-			temp = (byte) ~temp;
-		}
-
-		while (power < temp) {
-			power <<= 1;
-			power ^= 1;
-			counter++;
-		}
-
-		for (int i = 0; i < counter; i++) {
-			output.append((temp >> i) & 1);
-		}
-
-		if (output.length() <= 8){
-			output.append("0".repeat(8-output.length()));
+		for (int i = 0; i < 8; i++) {
+			output.append((x >>> i) & 1);
 		}
 		output.reverse();
-
-		if (x < 0){
-            return output.toString().replace("0", ".").replace("1", "0").replace(".", "1");
-		}
 		return output.toString();
 	}
 
